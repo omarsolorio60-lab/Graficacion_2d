82035993116bbe3312ac04849f74862b48628f6a
omarsolorio60-lab <omar.solorio60@uabc.edu.mx>
2025-10-07 08:39:43 -0700

    Delete math directory
    borrar para organizar

--- math/vectores.c
+++
@@ -1,9 +0,0 @@
-#include "vectores.h"
-#include <math.h>
-
-float distanciav2(Vec2 p1, Vec2 p2)
-{
-    return sqrt((p2.unpack.x-p1.unpack.x)*(p2.unpack.x-p1.unpack.x)
-                +
-                (p2.unpack.y-p1.unpack.y)*(p2.unpack.y-p1.unpack.y));
-}
