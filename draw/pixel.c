dbfb6e19df9c8c9ddbce74f9827850d1253dccee
omarsolorio60-lab <omar.solorio60@uabc.edu.mx>
2025-10-07 08:35:28 -0700

    Delete draw directory
    borrar para organizar

--- draw/pixel.c
+++
@@ -1,9 +0,0 @@
-#include "draw.h"
-#include "../global.h"
-
-void draw_pixel(int x, int y, uint32_t color)
-{
-    if((x<0 || y<0) ) return;
-    if( x>= estadosrender.ven_width || y >= estadosrender.ven_height) return;
-    estadosrender.color_buffer[y*estadosrender.ven_width+x] = color;
-}
